5b05f814400af27ca35bfbfa6289f38766116393
khwa16 <hatjikhalid@gmail.com>
2021-03-31 12:21:18 +0200

    first code

---
+++ lern/src/lern/hey.java
@@ -0,0 +1,23 @@
+package lern;
+
+public class hey {
+
+	private static int x;
+	private static int y;
+ 
+	public static void main(String[] args) {
+	System.out.println("Geben Sie eine Zahl ein");	
+	boolean check = true;
+do {
+	if(x>=1) {
+	 System.out.println("hey");
+	}
+	if(y>=2) {
+		System.out.println("hey");
+	}
+	
+	}while(check);
+		
+	}
+
+}
