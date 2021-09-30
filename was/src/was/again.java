96b5e2d1187679bf4a5249c43f05181acee7ed31
khwa16 <72856861+khwa16@users.noreply.github.com>
2021-09-30 11:54:19 +0200

    Update again.java

--- was/src/was/again.java
+++ was/src/was/again.java
@@ -5,6 +5,22 @@ public class again {
 	public static void main(String[] args) {
 System.out.println("dfghjkl");
 System.out.println("zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz");
+if(1<i)
+	if(1<i)
+if(1<i)
+if(1<i)
+if(1<i)
+if(1<i)
+if(1<i)
+if(1<i)
+if(1<i)
+if(1<i)
+if(1<i)
+if(1<i)
+if(1<i)
+if(1<i)
+if(1<i)
+if(1<i)
 
 	}
 
